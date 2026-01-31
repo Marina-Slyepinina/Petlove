4b6dbcda98fbf5ff81ceb0105e463cc08bab2bf1
Marina <marina.slepinina3@gmail.com>
2026-01-31 16:52:56 +0200

    updated UserBar

--- src/components/UserBar/UserBar.tsx
+++ src/components/UserBar/UserBar.tsx
@@ -1,15 +1,20 @@
-import { NavLink } from 'react-router'
+import { NavLink, useLocation } from 'react-router'
 import css from './UserBar.module.css'
+import clsx from 'clsx';
 
 export const UserBar = () => {
+
+  const location = useLocation();
+  const isHome = location.pathname === "/"
+
   return (
     <NavLink to="/profile" className={css.userBar}>
-        <div className={css.userIcon}>
-            <svg>
-                <use href='sprite.svg#user'></use>
-            </svg>
-        </div>
-        <p className={css.userName}>User name</p>
+      <div className={clsx(css.userIcon, isHome && css.userIconWhite)}>
+        <svg>
+          <use href='sprite.svg#user'></use>
+        </svg>
+      </div>
+      <p className={clsx(css.userName, isHome && css.userNameWhite)}>User name</p>
     </NavLink>
   )
 }
