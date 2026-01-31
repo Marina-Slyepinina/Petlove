8a8bfd9f5a608ad3a7d9969bc0c6b00199baa25f
Marina <marina.slepinina3@gmail.com>
2026-01-31 16:54:25 +0200

    updated UserNav

--- src/components/UserNav/UserNav.tsx
+++ src/components/UserNav/UserNav.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 import { UserBar } from '../UserBar/UserBar'
 import { ModalApproveAction } from '../ModalApproveAction/ModalApproveAction';
+import clsx from 'clsx';
 import css from './UserNav.module.css'
 
+interface UserNavProps {
+    isHome?: boolean;
+}
 
-export const UserNav = () => {
+export const UserNav = ({ isHome }: UserNavProps) => {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
         <div className={css.userNavContainer}>
 
-            <button type="button" onClick={() => setIsModalOpen(true)} className={css.btn}>Log out</button>
+            <button type="button" onClick={() => setIsModalOpen(true)} className={clsx(css.btn, isHome && css.outline)}>Log out</button>
             {isModalOpen && <ModalApproveAction onClose={() => setIsModalOpen(false)} />}
 
             <UserBar />
