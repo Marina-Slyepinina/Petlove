a9439406a1a4dfc33f9314fa49b4573c70f05469
Marina <marina.slepinina3@gmail.com>
2026-02-02 20:38:30 +0200

    added SearchField

---
+++ src/components/SearchField/SearchField.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import clsx from 'clsx';
+import css from './SearchField.module.css';
+
+interface SearchFieldProps {
+  onSearch: (query: string) => void;
+  placeholder?: string;
+  className?: string;
+}
+
+export const SearchField = ({ onSearch, placeholder, className }: SearchFieldProps) => {
+
+  const [query, setQuery] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  }
+
+  const handleClear = () => {
+    setQuery('');
+    onSearch('');
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSearch(query);
+    setQuery('');
+  }
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <input type="text" className={clsx(css.input, className)} placeholder={placeholder} value={query} onChange={handleChange}/>
+
+      <div className={css.actionsBlock}>
+        {query && <button type="button" onClick={handleClear}>
+          <svg className={css.clearSvg}>
+            <use href='sprite.svg#cross-small'></use>
+          </svg>
+        </button>}
+        <button type="submit">
+          <svg className={css.searchSvg}>
+            <use href='sprite.svg#search'></use>
+          </svg>
+        </button>
+      </div>
+    </form>
+  );
+};
