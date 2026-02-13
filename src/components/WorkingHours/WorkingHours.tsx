474f51bc8686e790d377c75e9596935371399965
Marina <marina.slepinina3@gmail.com>
2026-02-13 21:33:48 +0200

    added WorkingHours

---
+++ src/components/WorkingHours/WorkingHours.tsx
@@ -0,0 +1,40 @@
+import clsx from "clsx";
+import { formatWorkTime } from "../../lib/utils";
+import type { WorkDay } from "../../types/friends";
+import css from "./WorkingHours.module.css";
+
+interface WorkingHoursProps {
+    workDays: WorkDay[];
+}
+
+export const WorkingHours = ({ workDays }: WorkingHoursProps) => {
+
+    const { currentWorkTime, details } = formatWorkTime(workDays);
+
+    if (details.length === 0) {
+        return (
+            <div className={css.bage}>
+                <p className={css.text}>{currentWorkTime}</p>
+            </div>
+        )
+    }
+
+    return (
+        <div className={css.container}>
+            <div className={css.bage}>
+                <p>{currentWorkTime}</p>
+            </div>
+
+            <div className={css.tooltip}>
+                <ul className={css.tooltipList}>
+                    {details.map((detail, index) => (
+                        <li key={index} className={clsx(css.tooltipItem, detail.isToday && css.activeItem)}>
+                            <span>{detail.day}: </span>
+                            <span>{detail.time}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
