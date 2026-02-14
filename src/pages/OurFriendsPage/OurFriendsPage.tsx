0eed51bca2a56a348f307dc0fb1629d2a4994b77
Marina <marina.slepinina3@gmail.com>
2026-02-14 22:46:02 +0200

    updated OurFriendsPage

--- src/pages/OurFriendsPage/OurFriendsPage.tsx
+++ src/pages/OurFriendsPage/OurFriendsPage.tsx
@@ -1,6 +1,13 @@
+import { FriendsList } from "../../components/FriendsList/FriendsList";
+import { Title } from "../../components/Title/Title";
+import css from "./OurFriendsPage.module.css";
+
 const OurFriendsPage = () => {
   return (
-    <div>OurFriendsPage</div>
+    <div className={css.container}>
+      <Title title="Our friends" />
+      <FriendsList />
+    </div>
   )
 }
 export default OurFriendsPage;
