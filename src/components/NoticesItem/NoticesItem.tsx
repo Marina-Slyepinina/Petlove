78f4020c1a8b6d152ff7446c854cb7cff8e5b2da
Marina <marina.slepinina3@gmail.com>
2026-03-07 20:41:07 +0200

    updated NoticesItem

--- src/components/NoticesItem/NoticesItem.tsx
+++ src/components/NoticesItem/NoticesItem.tsx
@@ -2,68 +2,53 @@ import clsx from "clsx";
 import type { Note } from "../../types/notices";
 import css from "./NoticesItem.module.css";
 
-
-const demoData: Note = {
-    _id: "6589436d05a6bcd9b9379420",
-    species: "dog",
-    category: "sell",
-    price: 150,
-    title: "Golden Retriever Puppies",
-    name: "Max",
-    birthday: "2022-01-10",
-    comment: "Adorable puppy looking for a loving home.",
-    sex: "male",
-    location: "641ffcc1ae4e889a02d25ca5",
-    imgURL: "https://ftp.goit.study/img/pets/1.webp",
-    createdAt: "2023-12-11T10:43:28.477Z",
-    user: "6576e7d0c4cc99fc5ef94221",
-    popularity: 2,
-    updatedAt: "2023-12-25T11:41:12.493Z"
+interface NoticesItemProps {
+    note: Note;
 }
 
-export const NoticesItem = () => {
+export const NoticesItem = ({ note }: NoticesItemProps) => {
     return (
         <li className={css.container}>
-            <div className={css.imgWrapper}><img src={demoData.imgURL} alt={demoData.title} className={css.img} /></div>
+            <div className={css.imgWrapper}><img src={note.imgURL} alt={note.title} className={css.img} /></div>
             <div className={css.info}>
                 <div className={css.header}>
-                    <p className={css.title}>{demoData.title}</p>
+                    <p className={css.title}>{note.title}</p>
                     <div className={css.rating}>
                         <svg width={16} height={16}>
                             <use href="sprite.svg#star"></use>
                         </svg>
-                        <span>{demoData.popularity}</span>
+                        <span>{note.popularity}</span>
                     </div>
                 </div>
 
                 <ul className={css.chracteristicsList}>
                     <li className={css.chracteristicItem}>
                         <p className={css.chracteristicCategoty}>Name</p>
-                        <p className={css.chracteristicData}>{demoData.name}</p>
+                        <p className={css.chracteristicData}>{note.name}</p>
                     </li>
                     <li className={css.chracteristicItem}>
                         <p className={css.chracteristicCategoty}>Birthday</p>
-                        <p className={css.chracteristicData}>{demoData.birthday}</p>
+                        <p className={css.chracteristicData}>{note.birthday}</p>
                     </li>
                     <li className={css.chracteristicItem}>
                         <p className={css.chracteristicCategoty}>Sex</p>
-                        <p className={css.chracteristicData}>{demoData.sex}</p>
+                        <p className={css.chracteristicData}>{note.sex}</p>
                     </li>
                     <li className={css.chracteristicItem}>
                         <p className={css.chracteristicCategoty}>Species</p>
-                        <p className={css.chracteristicData}>{demoData.species}</p>
+                        <p className={css.chracteristicData}>{note.species}</p>
                     </li>
                     <li className={css.chracteristicItem}>
                         <p className={css.chracteristicCategoty}>Category</p>
-                        <p className={css.chracteristicData}>{demoData.category}</p>
+                        <p className={css.chracteristicData}>{note.category}</p>
                     </li>
                 </ul>
 
-                <p className={css.text}>{demoData.comment}</p>
+                <p className={css.text}>{note.comment}</p>
             </div>
 
             <div className={css.footer}>
-                <div className={css.price}>${demoData.price}</div>
+                <div className={css.price}>${note.price || ' Free'}</div>
                 <div className={css.btnsContainer}>
                     <button type="button" className={clsx(css.btn, css.btnMore)}>Learn more</button>
                     <button type="button" className={clsx(css.btn, css.btnLike)}>
@@ -74,5 +59,5 @@ export const NoticesItem = () => {
                 </div>
             </div>
         </li>
-    )   
+    )
 }
