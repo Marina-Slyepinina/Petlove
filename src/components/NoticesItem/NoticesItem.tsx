dd2289f6da0fb553985e5fbab972f78238dff93d
Marina <marina.slepinina3@gmail.com>
2026-03-17 19:46:35 +0200

    updated NoticesItem

--- src/components/NoticesItem/NoticesItem.tsx
+++ src/components/NoticesItem/NoticesItem.tsx
@@ -1,5 +1,10 @@
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 import type { Note } from "../../types/notices";
+import { useAuthStore } from "../../store/authStore";
+import { useFavoritesStore } from "../../store/favoritesStore";
+import { ModalAttention } from "../ModalAttention/ModalAttention";
+import { ModalNotice } from "../ModalNotice/ModalNotice";
 import css from "./NoticesItem.module.css";
 
 interface NoticesItemProps {
@@ -7,6 +12,44 @@ interface NoticesItemProps {
 }
 
 export const NoticesItem = ({ note }: NoticesItemProps) => {
+
+    const fetchFavorites = useFavoritesStore(state => state.fetchFavorites);
+    const addFavorite = useFavoritesStore(state => state.addFavorite);
+    const removeFavorite = useFavoritesStore(state => state.removeFavorite);
+    const favoriteIds = useFavoritesStore(state => state.favoriteIds);
+    const isFavorite = favoriteIds.find(item => item === note._id)
+
+    useEffect(() => {
+        fetchFavorites();
+    }, [fetchFavorites])
+
+    const [isModalAttentionOpen, setIsModalAttentionOpen] = useState(false);
+    const [isModalLearnMoreOpen, setIsModalLearnMoreOpen] = useState(false);
+
+    const isLoggedIn = useAuthStore(state => state.isLoggedIn);
+
+    const handleLearnMoreBtn = () => {
+        if (!isLoggedIn) {
+            setIsModalAttentionOpen(true)
+            return;
+        }
+        setIsModalLearnMoreOpen(true)
+    }
+
+    const handleLikeBtn = (id: string) => {
+        if (!isLoggedIn) {
+            setIsModalAttentionOpen(true)
+            return;
+        }
+
+        if (favoriteIds.includes(id)) {
+            removeFavorite(id);
+            return;
+        };
+
+        addFavorite(id);
+    }
+
     return (
         <li className={css.container}>
             <div className={css.imgWrapper}><img src={note.imgURL} alt={note.title} className={css.img} /></div>
@@ -50,14 +93,20 @@ export const NoticesItem = ({ note }: NoticesItemProps) => {
             <div className={css.footer}>
                 <div className={css.price}>${note.price || ' Free'}</div>
                 <div className={css.btnsContainer}>
-                    <button type="button" className={clsx(css.btn, css.btnMore)}>Learn more</button>
-                    <button type="button" className={clsx(css.btn, css.btnLike)}>
-                        <svg width={18} height={18}>
+                    <button type="button" className={clsx(css.btn, css.btnMore)} onClick={handleLearnMoreBtn}>Learn more</button>
+                    <button type="button" className={clsx(css.btn, css.btnLike)} onClick={() => handleLikeBtn(note._id)}>
+                        <svg width={18} height={18} className={clsx(isFavorite && css.filled)}>
                             <use href="sprite.svg#heart"></use>
                         </svg>
                     </button>
                 </div>
             </div>
+            {isModalAttentionOpen && (
+                <ModalAttention onClose={() => setIsModalAttentionOpen(false)} />
+            )}
+            {isModalLearnMoreOpen && (
+                <ModalNotice note={note} onClose={() => setIsModalLearnMoreOpen(false)} />
+            )}
         </li>
     )
 }
