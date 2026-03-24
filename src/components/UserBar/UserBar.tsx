fe7ab9f136215ea2668f4ee4b69307c580c5f4f3
Marina <marina.slepinina3@gmail.com>
2026-03-24 19:19:39 +0200

    updated UserBar

--- src/components/UserBar/UserBar.tsx
+++ src/components/UserBar/UserBar.tsx
@@ -1,20 +1,25 @@
-import { NavLink, useLocation } from 'react-router'
-import css from './UserBar.module.css'
 import clsx from 'clsx';
+import { NavLink, useLocation } from 'react-router'
+import { useAuthStore } from '../../store/authStore';
+import css from './UserBar.module.css'
 
 export const UserBar = () => {
 
   const location = useLocation();
   const isHome = location.pathname === "/"
 
+  const user = useAuthStore(state => state.user);
+  console.log(user);
+
   return (
     <NavLink to="/profile" className={css.userBar}>
       <div className={clsx(css.userIcon, isHome && css.userIconWhite)}>
-        <svg>
-          <use href='sprite.svg#user'></use>
-        </svg>
+        {user?.avatar ? <img src={user.avatar} alt="Avatar" className={css.userAvatar} />
+          : <svg>
+            <use href='sprite.svg#user'></use>
+          </svg>}
       </div>
-      <p className={clsx(css.userName, isHome && css.userNameWhite)}>User name</p>
+      <p className={clsx(css.userName, isHome && css.userNameWhite)}>{user?.name}</p>
     </NavLink>
   )
 }
