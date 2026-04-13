8cdf6db543880fa1b90743d275d2d4f19c3c0fc3
Marina <marina.slepinina3@gmail.com>
2026-04-13 16:53:37 +0300

    added handleDeletePet in petCard

--- src/components/petCard/petCard.tsx
+++ src/components/petCard/petCard.tsx
@@ -1,6 +1,9 @@
+import { useAuthStore } from "../../store/authStore";
+import { deletePet } from "../../lib/petsApi";
 import css from "./petCard.module.css";
 
 interface petCardProps {
+    _id: string,
     name: string,
     title: string,
     imgURL: string,
@@ -9,7 +12,19 @@ interface petCardProps {
     sex: string,
 }
 
-export const PetCard = ({ name, imgURL, title, birthday, sex, species }: petCardProps) => {
+export const PetCard = ({ _id, name, imgURL, title, birthday, sex, species }: petCardProps) => {
+
+    const removePetFromStore = useAuthStore(state => state.removeUserPet);
+
+    const handleDeletePet = async (id: string) => {
+        try {
+            await deletePet(id);
+            removePetFromStore(id);
+        } catch (error) {
+            console.error('Failed to delete pet: ', error);
+        }
+    };
+
     return (
         <li className={css.petCard}>
             <img className={css.petCardImage} src={imgURL} alt="pet photo" />
@@ -35,7 +50,7 @@ export const PetCard = ({ name, imgURL, title, birthday, sex, species }: petCard
                 </div>
             </div>
 
-            <button type="button" className={css.deleteBtn}>
+            <button type="button" className={css.deleteBtn} onClick={() => handleDeletePet(_id)}>
                 <svg width={16} height={16}>
                     <use href="sprite.svg#trash"></use>
                 </svg>
